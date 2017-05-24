34ba54b55bb7626b2371b82d164a49a31bc2b82b
mgunter-pivotal <mgunter@pivotal.io>
2017-05-23 21:28:13 -0400

    Update task.sh

--- run-maven-itests/task.sh
+++ run-maven-itests/task.sh
@@ -13,7 +13,7 @@ args=""
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
-./mvnw clean test
+./mvnw clean test $args
 #./mvnw verify $args
 
 cd ..
